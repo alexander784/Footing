e68694e33d91aff1885e40b17065b303c41bb9f9
alexander784 <alexandernyaga1999@gmail.com>
2024-09-26 08:46:12 +0300

    Create footer and home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
     <>
     <Navbar />
+<Router >
+    <Routes>
+    <Route path='/' element={<Home />}/>
+    </Routes>
+
+    </Router>
+    <Footer />
     </>
   );
 }
