216e9dda34b03adeddc510491ad4091712ecf542
alexander784 <alexandernyaga1999@gmail.com>
2024-10-01 13:58:27 +0300

    Handle fileupload logic

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Gallery = () => {
+  const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [imageURL, setImageURL] = useState([]);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (!file) return;
+
+    const storageRef = ref(storage, `image/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) *100;
+        setProgress(progress);
+        console.log('Upload is ' + progress + '% done');
+      },
+      (error) => {
+        console.error('Upload failed:', error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref)
+        .then((downloadURL) => {
+          setImageURL(downloadURL);
+          console.log('FIle available at', downloadURL);
+        });
+      }
+    );
+  };
+
+
+
+
   return (
-    <div>Gallerry</div>
+    <div></div>
   )
 }
 
